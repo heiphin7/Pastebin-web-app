c1b424fc408383c006568d7bc02866696be8f8d3
rshal <heiphin7and2@gmail.com>
2024-05-24 20:22:18 +0500

    Created new endpoints & added ResponseEntity using

--- src/main/java/pastebin/mainservice/controller/PasteController.java
+++ src/main/java/pastebin/mainservice/controller/PasteController.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pastebin.mainservice.entity.Paste;
 import pastebin.mainservice.service.PasteService;
+
+import java.nio.file.AccessDeniedException;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,14 +21,14 @@ public class PasteController {
     // TODO CUSTOM ERROR & SUCCES ENTITES FOR HTTP RESPONSE
 
     @PostMapping("/create/paste")
-    public String createNewPaste(@RequestParam Paste paste) {
+    public ResponseEntity<?> createNewPaste(@RequestParam Paste paste) {
         try {
             pasteService.save(paste);
-            return "paste успешно сохранен";
+            return ResponseEntity.ok("Paste успешно сохранен");
         } catch (NullPointerException exception) {
-            return "Все поля должны быть заполнены!";
+            return new ResponseEntity<>("Все поля должны быть заполнены!", HttpStatus.BAD_REQUEST);
         } catch (Exception exception) { // HTTP 500
-            return "Что то пошло не так: " + exception.getMessage();
+            return ResponseEntity.ok("Произошла какая-то ошибка!");
         }
     }
 
@@ -41,5 +44,32 @@ public class PasteController {
             return new ResponseEntity<>("Произошла какая-то ошибка", HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
+    }
+
+    @PutMapping("/change/paste")
+    public ResponseEntity<?> updatePaste(@RequestParam Paste paste, Long oldPasteId) {
+        try {
+            pasteService.updatePaste(oldPasteId, paste);
+            return ResponseEntity.ok("Paste успешно изменен");
+        } catch (ChangeSetPersister.NotFoundException exception) {
+            return new ResponseEntity<>("Paste для изменения не найден!", HttpStatus.NOT_FOUND);
+        } catch (Exception exception) { // HTTP 500
+            return new ResponseEntity<>("Произошла какая-то ошибка", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/paste/{paste_id}")
+    public ResponseEntity<?> deletePasteById(@PathVariable Long paste_id, Long user_id) {
+        try {
+            pasteService.deleteById(paste_id, user_id);
+            return ResponseEntity.ok("Paste успешно удалён!");
+        } catch (ChangeSetPersister.NotFoundException exception) {
+            return new ResponseEntity<>("Paste с id=" + paste_id + " не найден!", HttpStatus.NOT_FOUND);
+        } catch (AccessDeniedException exception) {
+            // Когда пользователь пытается удалить чужой paste
+            return new ResponseEntity<>("Вы не можете удалять чужие посты!", HttpStatus.FORBIDDEN);
+        } catch (Exception exception) { // HTTP 500
+            return new ResponseEntity<>("Произошла какая-то ошибка", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
